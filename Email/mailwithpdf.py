0abff0443b7aeab312a21380afafcc9ffa88d1db
criticalhitx <clink200032@gmail.com>
2021-03-26 22:53:33 +0700

    mail pdf

---
+++ Email/mailwithpdf.py
@@ -0,0 +1,54 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+ 
+body = '''Hello,
+This is the body of the email
+sicerely yours
+G.G.
+'''
+# put your email here
+sender = 'FILL WITH SENDER EMAIL'
+password = 'FILL WITH PASSWORD'
+# put the email of the receiver here
+receiver = 'WILL WITH RECEIVER EMAIL'
+ 
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender
+message['To'] = receiver
+message['Subject'] = 'This email has an attacment, a pdf file'
+ 
+message.attach(MIMEText(body, 'plain'))
+ 
+pdfname = 'GCP_Training.pdf'
+ 
+# open the file in bynary
+binary_pdf = open(pdfname, 'rb')
+ 
+payload = MIMEBase('application', 'octate-stream', Name=pdfname)
+# payload = MIMEBase('application', 'pdf', Name=pdfname)
+payload.set_payload((binary_pdf).read())
+ 
+# enconding the binary into base64
+encoders.encode_base64(payload)
+ 
+# add header with pdf name
+payload.add_header('Content-Decomposition', 'attachment', filename=pdfname)
+message.attach(payload)
+ 
+#use gmail with port
+session = smtplib.SMTP('smtp.gmail.com', 587)
+ 
+#enable security
+session.starttls()
+ 
+#login with mail_id and password
+session.login(sender, password)
+ 
+text = message.as_string()
+session.sendmail(sender, receiver, text)
+session.quit()
+print('Mail Sent')
